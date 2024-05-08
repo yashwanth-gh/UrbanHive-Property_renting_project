b75d47747b94a20f3ad5c33ac51d7cc76eeb011a
Yashwanth B M <yashwanthbm36@gmail.com>
2024-05-08 23:03:58 +0530

    added navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '@/assets/styles/globals.css'
+import Navbar from '@/components/Navbar'
 
 export const metadata = {
     title: 'UrbanHive | Together, at Home!',
@@ -11,6 +12,7 @@ const MainLayout = ({ children }) => {
     return (
         <html lang='en'>
             <body>
+                <Navbar />
                 <div>{children}</div>
             </body>
         </html>
---
+++ app/properties/add/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PropertiesAddPage = () => {
+    return (
+        <div>PropertiesAddPage</div>
+    )
+}
+
+export default PropertiesAddPage
